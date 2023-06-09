ff9025808f55c943782ab5d3b598835fb4170946
sahil <sahil@DESKTOP-92BED6E.domain.name>
2023-06-09 14:43:21 +0530

    project complete

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import React, { useEffect , useState } from 'react'
+import React, { useEffect  } from 'react'
 import { add } from '../store/CartSlice';
 import { useDispatch , useSelector } from 'react-redux';
 import { fetchProducts } from '../store/productSlice';
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 // using it as enum freeze make it read only
 export const STATUSES=Object.freeze({
@@ -15,13 +15,28 @@ const productSlice = createSlice({
         status:STATUSES.IDLE
     },
     reducers:{
-        setProducts(state,action){
-            // never do async function use in reducers
+        // setProducts(state,action){
+        //     // never do async function use in reducers
+        //     state.data=action.payload;
+        // },
+        // setStatus(state,action){
+        //     state.status=action.payload;
+        // }
+
+        // for toolkit thunk we have commited this and added extra reducers
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchProducts.pending,(state,action)=>{
+            state.status=STATUSES.LOADING
+        })
+        .addCase(fetchProducts.fulfilled,(state,action)=>{
             state.data=action.payload;
-        },
-        setStatus(state,action){
-            state.status=action.payload;
-        }
+            state.status=STATUSES.IDLE
+        })
+        .addCase(fetchProducts.rejected,(state,action)=>{
+            state.status=STATUSES.ERROR
+        })
     }   
 })
 
@@ -30,19 +45,28 @@ export default productSlice.reducer
 
 // Thunks
 // thunk is a programming term that means a peace of code that does same delayed work.
-export function fetchProducts(){
-    // thunk is a funtion and from which we return function
-    return async function fetchProductThunk(dispatch,getState){
-        dispatch(setStatus(STATUSES.LOADING))
-        try {
+// export function fetchProducts(){
+//     // thunk is a funtion and from which we return function
+//     return async function fetchProductThunk(dispatch,getState){
+//         dispatch(setStatus(STATUSES.LOADING))
+//         try {
+//             const response = await fetch('https://fakestoreapi.com/products')
+//             const data = await response.json()
+//             setProducts(data)
+//             dispatch(setProducts(data))
+//             dispatch(setStatus(STATUSES.IDLE))
+//         } catch (error) {
+//             console.log("Error !")
+//             dispatch(setStatus(STATUSES.ERROR))
+//         }
+//     }
+// }
+// above thunk was basic and by redux ,in redux toolkit we have another
+
+export const fetchProducts = createAsyncThunk('products/fetch',async()=>{
+    // first parameter was simply a identifier and 2nd is async fun
+
             const response = await fetch('https://fakestoreapi.com/products')
             const data = await response.json()
-            setProducts(data)
-            dispatch(setProducts(data))
-            dispatch(setStatus(STATUSES.IDLE))
-        } catch (error) {
-            console.log("Error !")
-            dispatch(setStatus(STATUSES.ERROR))
-        }
-    }
-}
+            return data;
+})
